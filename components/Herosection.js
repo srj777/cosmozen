0ca8946e6fd14b0655bdc2b8eb34697eb3d42bfb
srj777 <39049653+srj777@users.noreply.github.com>
2023-12-29 08:10:19 +0530

    Update Herosection.js

--- components/Herosection.js
+++ components/Herosection.js
@@ -9,11 +9,10 @@ function Herosection() {
           <div class="text-wrapper col-12 col-lg-7">
             <div class="text-body">
               <h1 class=" fw-bold">
-                The easiest way to manage projects
+                COMING SOON...
               </h1>
               <p class="mt-3">
-                From the small stuff to the big picture, organizes the work so
-                teams know what to do, why it matters, and how to get it done.
+                Cosmozen - The cosmic generation. Live like a multi Galactic species.
               </p>
             </div>
             <div class="btns-group mt-3">
